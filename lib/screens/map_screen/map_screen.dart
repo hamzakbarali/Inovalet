c8536bb99f4443192fb3ad332b023d4454d4160a
Syed Hamza Akbar Ali <hamzakbarali@gmail.com>
2022-01-14 10:25:13 +0500

    Added google maps flutter imports in pubspec file and index.html in flutter web folder

--- lib/screens/map_screen/map_screen.dart
+++ lib/screens/map_screen/map_screen.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
