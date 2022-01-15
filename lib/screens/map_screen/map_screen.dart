edba4979663b7434d336f6cc13bf7ebcd956b57f
Syed Hamza Akbar Ali <hamzakbarali@gmail.com>
2022-01-15 11:00:09 +0500

    Added google maps code to map screen

--- lib/screens/map_screen/map_screen.dart
+++ lib/screens/map_screen/map_screen.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
-import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import "package:google_fonts/google_fonts.dart";
+import "../../utils/utils_barrel.dart";
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
@@ -10,10 +11,27 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  CameraPosition camPos =
+      const CameraPosition(target: LatLng(24.8200, 67.0307), zoom: 18);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: brownColor,
+        title: Text(
+          "Choose a Location",
+          style: GoogleFonts.lobster(
+            textStyle: const TextStyle(),
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: GoogleMap(
+          initialCameraPosition: camPos,
+          myLocationEnabled: true,
+        ),
+      ),
     );
   }
 }
