157dde426c2af6b7efcb8b4b28df871f03696a77
Syed Hamza Akbar Ali <hamzakbarali@gmail.com>
2022-01-23 08:56:52 +0500

    Added circular timer and duration picker to call valet screen

--- lib/screens/call_valet_map_screen/call_valet_map_screen.dart
+++ lib/screens/call_valet_map_screen/call_valet_map_screen.dart
@@ -1,9 +1,10 @@
-import 'dart:typed_data';
 import "package:flutter/material.dart";
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import "package:google_fonts/google_fonts.dart";
 import "../../utils/utils_barrel.dart";
 import "package:location/location.dart";
+import "package:duration_picker/duration_picker.dart";
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 
 class CallValetMapScreen extends StatefulWidget {
   const CallValetMapScreen({Key? key}) : super(key: key);
@@ -19,6 +20,7 @@ class _CallValetMapScreenState extends State<CallValetMapScreen> {
   late LatLng _valetPos;
   late Set<Marker> _markers;
   late LatLng _pickedLoc;
+  late Duration? _duration;
 
   @override
   void initState() {
@@ -28,6 +30,7 @@ class _CallValetMapScreenState extends State<CallValetMapScreen> {
         const CameraPosition(target: LatLng(24.8200, 67.0307), zoom: 18.2);
     _valetPos = const LatLng(24.820019, 67.030432);
     _markers = Set<Marker>();
+    _duration = null;
     setMarkers();
   }
 
@@ -59,23 +62,46 @@ class _CallValetMapScreenState extends State<CallValetMapScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniCenterFloat,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: brownColor,
+        onPressed: () async {
+          _duration = await showDurationPicker(
+            context: context,
+            initialTime: const Duration(minutes: 20),
+            decoration: BoxDecoration(
+                color: grey300,
+                borderRadius: const BorderRadius.all(Radius.circular(20))),
+          );
+        },
+        child: Icon(
+          Icons.timer,
+          color: pinkColor,
+        ),
+      ),
       appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: brownColor,
-          title: Text(
-            "Calling Valet",
-            style: GoogleFonts.lobster(
-              textStyle: const TextStyle(),
-            ),
+        centerTitle: true,
+        backgroundColor: brownColor,
+        title: Text(
+          "Calling Valet",
+          style: GoogleFonts.lobster(
+            textStyle: const TextStyle(),
           ),
-          actions: []),
+        ),
+      ),
       body: SafeArea(
-        child: GoogleMap(
-          zoomGesturesEnabled: true,
-          markers: _markers,
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: _camPos,
-          myLocationEnabled: true,
+        child: Stack(
+          children: [
+            GoogleMap(
+              zoomGesturesEnabled: true,
+              markers: _markers,
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: _camPos,
+              myLocationEnabled: true,
+            ),
+            const Positioned(child: Icon(Icons.add), top: 10, right: 10),
+          ],
         ),
       ),
     );
